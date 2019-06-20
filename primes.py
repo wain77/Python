d317401283bf5c07bf38d0082f12f8fd1eb91568
wain77 <samwain@gmail.com>
2019-06-20 16:36:40 +0100

    Create a script to count how many primes up to the

---
+++ primes.py
@@ -0,0 +1,13 @@
+primes = []
+upperLimit = 10
+for possiblePrime in range(2,upperLimit):
+	isPrime = True
+	for num in range(2, possiblePrime):
+		if possiblePrime % num == 0:
+			isPrime = False
+			break
+		
+	if isPrime:
+		primes.append(possiblePrime)
+
+print(len(primes))
